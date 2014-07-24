28c61e15b6444ba39bcb45852a37f256081b6cd4
sfmiller940 <sfm15@columbia.edu>
2014-07-24 11:19:57 -0700

    Updates.

--- beta/AnonBB.js
+++ beta/AnonBB.js
@@ -2,41 +2,77 @@
 
 function anonBB(BB_divID){
 
+	// Primary divs
+	// Append error div
+	$(BB_divID).append('<div id="error"></div>');
 	// Append menu div
 	$(BB_divID).append('<div id="menu"></div>');
-
-	// Append all threads button
-	$(BB_divID + ' #menu').append('<a class="button" id="all_threads">All Threads</a>');
-	$("#all_threads").click(function(e) {
-	    e.preventDefault();
-	    showThreads();
-	    return false;  
-	});
-
-	// Append new thread button
-	$(BB_divID + ' #menu').append('<a class="button" id="new_threads">New Threads</a>');
-
-	// Append threads
+	// Append threads div
 	$(BB_divID).append('<div id="threads"></div>');
-
-	// Show threads
-	showThreads = function(){
-		$.get("AnonBB.php?get_threads", function(data) {
-			//var threads = jQuery.parseJSON(data);
-			$(BB_divID + ' #threads').append(data);
-		});
-	}
-	
-	// Append posts
+	// Append new_thread div
+	$(BB_divID).append('<div id="new_thread"></div>');
+	// Append posts div
 	$(BB_divID).append('<div id="posts"></div>');
 
-	// Show posts
-	showPosts = function(){
-		$.get("AnonBB.php?get_posts", function(data) {
-			//var threads = jQuery.parseJSON(data);
-			$(BB_divID + ' #posts').append(data);
+	// Clear divs
+	function clear_most(){
+		$('#error').empty();
+		$('#threads').empty();
+		$('#new_thread').empty();
+		$('#posts').empty();
+	}
+
+
+	// Functions
+	// Show threads
+	showThreads = function(){
+		clear_most();
+		$.get("AnonBB.php?get_threads", function(data) {
+			var threads = jQuery.parseJSON(data);
+			$.each(threads, function(i, thread) {
+				$(BB_divID + ' #threads').append('<div class="thread" id="thread_'+ thread.ID +'"></div>');
+				$(BB_divID + ' #threads #thread_' + thread.ID).append(
+					'<div class="user">' + thread.User + '</div>' +
+					'<a class="subject" id="view_thread_'+ thread.ID +'" href="View Thread">'+ thread.Subject +'</a>' +
+					'<div class="date">' + thread.Posted + '</div>'
+				);
+				$("#view_thread_" + thread.ID).click(function(e) {
+				    e.preventDefault();
+				    showPosts(thread.ID);
+				    return false;  
+				});
+			});
 		});
 	}
+	// Append show threads button to menu
+	$(BB_divID + ' #menu').append('<input type="button" value="All Threads" id="all_threads">');
+	$('#all_threads').click(showThreads);
+	
+
+	// Show posts
+	showPosts = function(ID){
+		clear_most();
+		$.get("AnonBB.php?get_posts&ID=" + ID, function(data) {
+			var posts = jQuery.parseJSON(data);
+			$.each(posts, function(i, post) {
+				$(BB_divID + ' #posts').append('<div class="post" id="post_'+ post.ID +'"></div>');
+				$(BB_divID + ' #posts #post_' + post.ID).append(
+					'<div class="user">' + post.User + '</div>' +
+					'<div class="date">'+ post.Posted +'</div>' +
+					'<div class="post">' + post.Post + '</div>'
+				);
+			});
+		});
+	}
+
+
+	// Display form for new thread
+	threadForm = function(){
+		clear_most();
+	}
+	// Append new thread button to menu
+	$(BB_divID + ' #menu').append('<input type="button" value="New Thread" id="new_thread">');
+	$('#new_thread').click( threadForm );
 
 
 	showThreads();
--- beta/AnonBB.php
+++ beta/AnonBB.php
@@ -18,27 +18,35 @@ if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
+// Convert sequel query results to JSON.
 function query_to_json($mysqli, $querystring){
 	if ( $sql_result = $mysqli->query( $querystring ) ){	
 		while ($r=$sql_result->fetch_assoc()){ $rows[] = $r; }
-		print json_encode($rows);
-		die();
+		return json_encode($rows);
 	}
 	else { die("SQL bad times!"); }
 }
 
 // If get_threads, return all threads as JSON.
-if ( isset($_GET['get_threads']) )
-	{ query_to_json($mysqli, "select * from `bdThreads` order by `Posted` desc"); }
+if ( isset($_GET['get_threads']) ){
+	$threads = query_to_json($mysqli, "select * from `bdThreads` order by `Posted` desc");
+	print $threads;
+	die();
+}
 
 // Get single thread
-else if (isset($_GET['get_posts']))
-	{ query_to_json($mysqli, "select * from `bdPosts` where `ID` = '".$mysqli->real_escape_string($_GET['ID'])."' order by `Posted`"); }
+else if (isset($_GET['get_posts'])){
+	$posts = query_to_json($mysqli, "select * from `bdPosts` where `ID` = '".$mysqli->real_escape_string($_GET['ID'])."' order by `Posted`");
+	print $posts;
+	die();
+}
 
 // Create new thread
 else if (isset($_GET['new_thread'])){}
 
 // Create new post.
 else if (isset($_GET['new_post'])){}
+
+// Install if not intalled... Redirect.
 
 ?>
