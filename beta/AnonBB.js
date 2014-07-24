f39bac2998d2ab9f79c7de845bcd4ff1d31276bd
sfmiller940 <sfm15@columbia.edu>
2014-07-23 21:01:12 -0700

    Updates

--- beta/AnonBB.js
+++ beta/AnonBB.js
@@ -2,12 +2,42 @@
 
 function anonBB(BB_divID){
 
-	function showThreads(){
+	// Append menu div
+	$(BB_divID).append('<div id="menu"></div>');
+
+	// Append all threads button
+	$(BB_divID + ' #menu').append('<a class="button" id="all_threads">All Threads</a>');
+	$("#all_threads").click(function(e) {
+	    e.preventDefault();
+	    showThreads();
+	    return false;  
+	});
+
+	// Append new thread button
+	$(BB_divID + ' #menu').append('<a class="button" id="new_threads">New Threads</a>');
+
+	// Append threads
+	$(BB_divID).append('<div id="threads"></div>');
+
+	// Show threads
+	showThreads = function(){
 		$.get("?get_threads", function(data) {
-			var threads = jQuery.parseJSON(data);
-			$(BB_divID).append(data);
+			//var threads = jQuery.parseJSON(data);
+			$(BB_divID + ' #threads').append(data);
 		});
 	}
 	
+	// Append posts
+	$(BB_divID).append('<div id="posts"></div>');
+
+	// Show posts
+	showPosts = function(){
+		$.get("?get_posts", function(data) {
+			//var threads = jQuery.parseJSON(data);
+			$(BB_divID + ' #posts').append(data);
+		});
+	}
+
+
 	showThreads();
 }
--- beta/index.php
+++ beta/index.php
@@ -1,16 +1,6 @@
 <?php
-
-// Display errors.
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
-
-// Config parameters
-$bb_URL = 'http://localhost:8888/beta/AnonBB';
-$db_host = 'localhost';
-$db_un = 'root';
-$db_pw = 'root';
-$db_db = 'AnonBB';
+// Get configuration
+require 'config.php';
 
 // Connect to Database
 $mysqli = new mysqli($db_host, $db_un, $db_pw, $db_db);
@@ -18,16 +8,22 @@ if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
-// Get all threads and return as JSON.
-if (isset($_GET['get_threads'])){
-	$sqlthreads = $mysqli->query("select * from `bdThreads` order by `Posted` desc");
-	while ($r=$sqlthreads->fetch_assoc()){ $rows[] = $r; }
-	print json_encode($rows);
-	die();
+function query_to_json($mysqli, $querystring){
+	if ( $sql_result = $mysqli->query( $querystring ) ){	
+		while ($r=$sql_result->fetch_assoc()){ $rows[] = $r; }
+		print json_encode($rows);
+		die();
+	}
+	else { die("SQL bad times!"); }
 }
 
+// If get_threads, return all threads as JSON.
+if ( isset($_GET['get_threads']) )
+	{ query_to_json($mysqli, "select * from `bdThreads` order by `Posted` desc"); }
+
 // Get single thread
-else if (isset($_GET['get_thread'])){}
+else if (isset($_GET['get_posts']))
+	{ query_to_json($mysqli, "select * from `bdPosts` where `ID` = '".$mysqli->real_escape_string($_GET['ID'])."' order by `Posted`"); }
 
 // Create new thread
 else if (isset($_GET['new_thread'])){}
@@ -41,6 +37,7 @@ else{
 <title>AnonBB</title>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="AnonBB.js"></script>
+<link rel="stylesheet" type="text/css" href="AnonBB.css">
 </head>
 <body>
 
