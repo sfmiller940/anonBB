e60d48ba84f4725b60f2ecdc3d23da78cf220016
sfmiller940 <sfm15@columbia.edu>
2014-07-25 10:25:58 -0700

    Updates

--- beta/AnonBB.js
+++ beta/AnonBB.js
@@ -3,8 +3,6 @@
 function anonBB(BB_divID){
 
 	// Primary divs
-	// Append error div
-	$(BB_divID).append('<div class="error"></div>');
 	// Append menu div
 	$(BB_divID).append('<div class="menu"></div>');
 	// Append threads div
@@ -12,17 +10,15 @@ function anonBB(BB_divID){
 	// Append threads div
 	$(BB_divID).append('<div class="new_content"></div>');
 
-	// Clear divs
+	// Clear content/new_content divs
 	function clear_most(){
-		$(BB_divID + ' .error').empty();
 		$(BB_divID + ' .content').empty();
 		$(BB_divID + ' .new_content').empty();
 	}
 
-
-	// Functions
 	// Show threads
 	showThreads = function(){
+		$(BB_divID + ' .title').html( 'AnonBB' );
 		clear_most();
 		$.get("AnonBB.php?get_threads", function(data) {
 			var threads = jQuery.parseJSON(data);
@@ -41,9 +37,9 @@ function anonBB(BB_divID){
 			});
 		});
 		// New thread div
-		$(BB_divID + ' .new_content').append( new_content("thread") );
+		$(BB_divID + ' .new_content').append( new_content_form("thread") );
 		// Submit new thread.
-        $("#make_thread").click( function () {
+        $(BB_divID + " .make_thread").click( function () {
           	$.post( 'AnonBB.php?new_thread', $("#new_content_form").serialize(), 
 	            function(data){
 	            	if (data == 2){ showThreads(); }
@@ -52,13 +48,10 @@ function anonBB(BB_divID){
           	);
         });   
 	}
-	// Append show threads button to menu
-	$(BB_divID + ' .menu').append('<input type="button" value="All Threads" class="all_threads">');
-	$(BB_divID + ' .all_threads').click(showThreads);
-	
 
 	// Show posts
 	showPosts = function(ID){
+		$(BB_divID + ' .title').html( $(BB_divID + ' a.view_thread_'+ ID).html() );
 		clear_most();
 		$.get("AnonBB.php?get_posts&ID=" + ID, function(data) {
 			var posts = jQuery.parseJSON(data);
@@ -71,7 +64,7 @@ function anonBB(BB_divID){
 				);
 			});
 		});
-		$(BB_divID + ' .new_content').append( new_content( ID ) );
+		$(BB_divID + ' .new_content').append( new_content_form( ID ) );
 		// Submit new thread.
         $(BB_divID + " .make_thread").click( function () {
           	$.post( 'AnonBB.php?new_post', $("#new_content_form").serialize(), 
@@ -84,24 +77,25 @@ function anonBB(BB_divID){
 
 	}
 
-
-	// Display form for new thread
-	newThread = function(){
-		clear_most();
+	// New content form(s)
+	function new_content_form (id){
+		var new_form = '<form id="new_content_form">' + 
+			'<label class="user">Name:</label><input name="User" type="text" id="user">';
+		if (id == 'thread'){ new_form +='<label class="user">Subject:</label><input name="Subject" type="text" id="subject">'; } 
+		new_form += '<label class="message">Message:</label><textarea name="Message" rows="4" cols="30" id="message"></textarea>' +
+			'<img id="captcha" src="securimage/securimage_show.php" alt="CAPTCHA Image" />' +
+			'<label class="captcha">Captcha:</label><input type="text" id="captcha_code" name="captcha_code" size="10" maxlength="6" />' +
+			'<input type="button" value="Post" class="make_thread">';
+		if (id != 'thread'){ new_form += '<input type="hidden" value="'+ id +'" name="ID">'; }
+		new_form += '</form>';
+		return new_form;
 	}
 
-	showThreads();
-}
+	// Append show threads button to menu
+	$(BB_divID + ' .menu').append('<p class="title"></p>');
+	$(BB_divID + ' .menu').append('<input type="button" value="All Threads" class="all_threads">');
+	$(BB_divID + ' .all_threads').click(showThreads);
 
-function new_content (id){
-	var new_form = '<form id="new_content_form">' + 
-		'<label class="user">Name:</label><input name="User" type="text" id="user">';
-	if (id == 'thread'){ new_form +='<label class="user">Subject:</label><input name="Subject" type="text" id="subject">'; } 
-	new_form += '<label class="message">Message:</label><textarea name="Message" rows="4" cols="30" id="message"></textarea>' +
-		'<img id="captcha" src="securimage/securimage_show.php" alt="CAPTCHA Image" />' +
-		'<label class="captcha">Captcha:</label><input type="text" id="captcha_code" name="captcha_code" size="10" maxlength="6" />' +
-		'<input type="button" value="Post" class="make_thread">';
-	if (id != 'thread'){ new_form += '<input type="hidden" value="'+ id +'" name="ID">'; }
-	new_form += '</form>';
-	return new_form;
+	// Show threads
+	showThreads();
 }
